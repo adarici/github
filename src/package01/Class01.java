5775e9eb6b60e8d8563f782ac8cde5b4ecd7c964
your name <comdarici@gmail.com>
2021-08-02 10:29:39 +0200

    third commit

--- src/package01/Class01.java
+++ src/package01/Class01.java
@@ -5,6 +5,7 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Batch 35 / 36 Learn Github");
+        System.out.println("We like git but we will like more when we have more practice");
 
     }
 }
