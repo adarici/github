db9d03eec006d7ac6f54613a415376f039727f56
your name <comdarici@gmail.com>
2021-08-02 10:27:05 +0200

    second commit

--- src/package01/Class01.java
+++ src/package01/Class01.java
@@ -4,6 +4,7 @@ public class Class01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Batch 35 / 36 Learn Github");
 
     }
 }
