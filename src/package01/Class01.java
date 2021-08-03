b45865654a9d96d540de6fbf9cc74dd7dec37c34
Ali Darici <comdarici@gmail.com>
2021-08-03 15:57:07 +0200

    third commit

--- src/package01/Class01.java
+++ src/package01/Class01.java
@@ -5,7 +5,8 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Batch 35 / 36 Learn Github");
-       System.out.println("We like git but we will like more when we have more practice ");
+        System.out.println("We like git but we will like more when we have more practice ");
+
 
     }
 }
