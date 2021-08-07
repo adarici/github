7d75b437d3040650cda340e827dc21445319149a
Ali Darici <comdarici@gmail.com>
2021-08-07 21:12:18 +0200

    last version

--- src/Class02.java
+++ src/Class02.java
@@ -1,7 +1,12 @@
 public class Class02 {
 
+    public static void main(String[] args) {
+        int x =50;
+        int sum = x+20;
+        System.out.println(sum);
+        int y = 100;
 
 System.out.println("THIS IS REMOTE REPO CODE");
-
+}
 
 }
