4919c08118064b2fd13dc0749e6d5ecc3affa7c9
Ali Darici <comdarici@gmail.com>
2021-08-07 21:08:17 +0200

    my fourth class

---
+++ src/MyFourthClass.java
@@ -0,0 +1,8 @@
+public class MyFourthClass {
+    public static void main(String[] args) {
+        int x =50;
+        int sum = x+20;
+        System.out.println(sum);
+        int y = 100;
+    }
+}
