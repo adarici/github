5eb118c615057b5b982bb10e96cf366d10e6601c
Ali Darici <comdarici@gmail.com>
2021-08-07 20:29:45 +0200

    Commiting my third Class

--- src/package01/Class01Settar.java
+++ src/package01/Class01Settar.java
@@ -1,6 +1,6 @@
 package package01;
 
-public class Class01 {
+public class Class01Settar {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ src/package01/class03.java
@@ -0,0 +1,10 @@
+package package01;
+
+public class class03 {
+    public static void main(String[] args) {
+        int x =5;
+        int sum = x+2;
+        System.out.println(sum);
+        int y = 0;
+    }
+}
