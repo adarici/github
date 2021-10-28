0c5f8b21d38fc45113d94647aa43462d49ee93db
adarici <88286697+adarici@users.noreply.github.com>
2021-10-28 23:26:00 +0200

    Update Class01Settar.java

--- src/package01/Class01Settar.java
+++ src/package01/Class01Settar.java
@@ -6,6 +6,8 @@ public class Class01Settar {
         System.out.println("Hello World");
         System.out.println("Batch 35 / 36 Learn Github");
         System.out.println("We like git but we will like more when we have more practice ");
+        
+        System.out.println("I don't like git but I need more and more practice ");
 
 
     }
